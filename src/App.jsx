7b60ee9e880c021e8333a100c166f037cc8fea50
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-04-02 06:31:41 +0800

    Create boilerplate for react

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+// TODO: Do not forget to import things melio!
+
+export default function App() {
+  return (
+    <main>
+      {/* TODO: Add the components here */}
+    </main>
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import '../styles/Card.css'
+
+export default function Card() {
+  return (
+    <div>
+      {/* TODO: Start here */}
+    </div>
+  )
+}
