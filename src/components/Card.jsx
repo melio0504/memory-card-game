d0c660c83887b7a2faa01759a583b028973a2726
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-04-02 16:30:18 +0800

    Create Card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,48 @@
-import '../styles/Card.css'
+export default function Card({ card, onClick, disabled }) {
+  const handleMouseMove = (event) => {
+    const rect = event.currentTarget.getBoundingClientRect()
+    const x = ((event.clientX - rect.left) / rect.width) * 100
+    const y = ((event.clientY - rect.top) / rect.height) * 100
+    const centerOffsetX = (x - 50) / 50
+    const centerOffsetY = (y - 50) / 50
+    const maxTilt = 14
 
-export default function Card() {
+    const rotateY = centerOffsetX * maxTilt
+    const rotateX = centerOffsetY * -maxTilt
+
+    event.currentTarget.style.setProperty('--mx', `${x}%`)
+    event.currentTarget.style.setProperty('--my', `${y}%`)
+    event.currentTarget.style.setProperty('--rx', `${rotateX}deg`)
+    event.currentTarget.style.setProperty('--ry', `${rotateY}deg`)
+    event.currentTarget.style.setProperty('--ty', '-8px')
+  }
+
+  const resetHover = (event) => {
+    event.currentTarget.style.removeProperty('--mx')
+    event.currentTarget.style.removeProperty('--my')
+    event.currentTarget.style.removeProperty('--rx')
+    event.currentTarget.style.removeProperty('--ry')
+    event.currentTarget.style.removeProperty('--ty')
+  }
+
   return (
-    <div>
-      {/* TODO: Start here */}
-    </div>
+    <button
+      type="button"
+      className="card"
+      onClick={() => onClick(card)}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={resetHover}
+      disabled={disabled}
+      aria-label={`Choose ${card.imageKey}`}
+    >
+      <img
+        src={card.imageSrc}
+        alt={card.alt}
+        className="card__image"
+        loading="lazy"
+        draggable={false}
+      />
+      <span className="card__shine" aria-hidden="true" />
+    </button>
   )
 }
